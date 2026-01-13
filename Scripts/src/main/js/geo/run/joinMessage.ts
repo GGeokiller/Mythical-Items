d84b75fc7d5dd049661f4b22056dd4b845908477
GGeokiller <geokilleroficial@gmail.com>
2026-01-12 23:13:08 -0700

    admin message

--- Scripts/src/main/js/geo/run/joinMessage.ts
+++ Scripts/src/main/js/geo/run/joinMessage.ts
@@ -2,10 +2,14 @@ import { world } from "@minecraft/server";
 import { addonInfo } from "../../addon/info";
 
 const JOIN_MESSAGE = `§9${addonInfo.title} v${addonInfo.version} (${addonInfo.mcversion}) Loaded Successfully by Geokiller`
+const ADMIN_MESSAGE = `§8[§9${addonInfo.title}§8] §cYou have admin permissions! Use §e/config§c to access the configuration panel.`
 
 world.afterEvents.playerSpawn.subscribe(({player, initialSpawn}) => {
     if (!initialSpawn) return;
     player.sendMessage(JOIN_MESSAGE);
+    if (player.playerPermissionLevel >= 2 || player.commandPermissionLevel >= 1) {
+        player.sendMessage(ADMIN_MESSAGE);
+    }
 })
 
 console.warn(`${addonInfo.title} (${addonInfo.mcversion}) Loaded Successfully`);
